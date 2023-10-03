50efd4c1a3993af838e11d000f800fa663c8b0ea
lucanevess87 <lucatets@hotmail.com>
2023-10-02 22:58:21 -0300

    fix/map-box

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,15 @@
 export const APP_NAME = 'vscode-luca';
 
-export const API_URL = '';
-
-export const GITHUB_ID = process.env.GITHUB_ID as string;
-export const GITHUB_SECRET = process.env.GITHUB_SECRET as string;
-export const NEXTAUTH_URL = process.env.NEXTAUTH_URL as string;
-export const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET as string;
-
-export const ENVIRONMENT = process.env.NODE_ENV;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
+export const GITHUB_ID = process.env.NEXT_PUBLIC_GITHUB_ID as string;
+export const GITHUB_SECRET = process.env.NEXT_PUBLIC_GITHUB_SECRET as string;
+export const NEXTAUTH_URL = process.env.NEXT_PUBLIC_NEXTAUTH_URL as string;
+export const NEXTAUTH_SECRET = process.env.NEXT_PUBLIC_NEXTAUTH_SECRET as string;
+export const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN as string;
+export const ENVIRONMENT = process.env.NODE_ENV as string;
 export const MOCK_ENABLED = process.env.NEXT_PUBLIC_API_MOCKING as string;
 
 export const DEFAULT_PUBLIC_PAGE = '/login';
-
 export const DEFAULT_PRIVATE_PAGE = '/';
 
 export const ACCESS_TOKEN_ID = 'personal-leap_access';
@@ -26,5 +24,3 @@ export const JWT_REFRESH_SECRET = '123456789' as string;
 export const JWT_REFRESH_EXPIRES_IN = 50;
 
 export const IS_CLIENT_SERVER = typeof window !== 'undefined';
-export const MAPBOX_TOKEN =
-  'pk.eyJ1IjoidGVjaC1sb29taSIsImEiOiJjbGtiN29iYmMwYmNzM2VueHpuMDVsNzU0In0.ES0Sx98e8dS-IgjSpjmSOw';
